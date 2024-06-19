8a7a07343c514d7ffc6a9d98fa1c78c1e854ca58
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2024-06-19 14:55:06 +0000

    random card generator by cesaramcolson

--- src/app.js
+++ src/app.js
@@ -5,7 +5,65 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateRandomIcon = () => {
+  let icons = ["♦", "♥", "♠", "♣"];
+  let indexIcons = Math.floor(Math.random() * icons.length);
+  return icons[indexIcons];
 };
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateNewCard = () => {
+  let randomIcon = generateRandomIcon();
+  let grabIcon = document.getElementsByClassName("icon");
+  for (let i = 0; i < grabIcon.length; i++) {
+    grabIcon[i].innerHTML = randomIcon;
+    if (randomIcon === "♦" || randomIcon === "♥") {
+      grabIcon[i].style.color = "red";
+    } else {
+      grabIcon[i].style.color = "black";
+    }
+  }
+
+  document.querySelector("#number").innerHTML = generateRandomNumber();
+
+  let width = document.getElementById("widthInput").value;
+  let height = document.getElementById("heightInput").value;
+
+  let card = document.getElementById("card");
+  card.style.width = width + "px";
+  card.style.height = height + "px";
+};
+
+let startTimer = () => {
+  setInterval(() => {
+    generateNewCard();
+  }, 5000);
+};
+
+window.onload = () => {
+  generateNewCard();
+  startTimer();
+};
+
+document
+  .getElementById("generateButton")
+  .addEventListener("click", generateNewCard);
