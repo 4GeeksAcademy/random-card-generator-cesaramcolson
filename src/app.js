4fc44c6681da7ccab5136b16b3e8ce09bd96715d
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2024-06-19 23:35:41 +0000

    added new dinamic resize of the card and rotate the icon on the bottom right corner

--- src/app.js
+++ src/app.js
@@ -31,6 +31,25 @@ let generateRandomNumber = () => {
   return numbers[indexNumbers];
 };
 
+let sizeRelation = 250 / 370;
+function updateHeightBasedOnWidth() {
+  let widthInput = document.getElementById("widthInput");
+  let heightInput = document.getElementById("heightInput");
+  let width = parseFloat(widthInput.value);
+  let height = width / sizeRelation;
+  heightInput.value = height.toFixed(2);
+  generateNewCard();
+}
+
+function updateWidthBasedOnHeight() {
+  let widthInput = document.getElementById("widthInput");
+  let heightInput = document.getElementById("heightInput");
+  let height = parseFloat(heightInput.value);
+  let width = height * sizeRelation;
+  widthInput.value = width.toFixed(2);
+  generateNewCard();
+}
+
 let generateNewCard = () => {
   let randomIcon = generateRandomIcon();
   let grabIcon = document.getElementsByClassName("icon");
@@ -51,7 +70,20 @@ let generateNewCard = () => {
   let card = document.getElementById("card");
   card.style.width = width + "px";
   card.style.height = height + "px";
+
+  let iconSize = calculateFontSize(width, height, 0.3);
+  let numberSize = calculateFontSize(width, height, 0.4);
+
+  for (let icon of grabIcon) {
+    icon.style.fontSize = iconSize + "px";
+  }
+  document.querySelector("#number").style.fontSize = numberSize + "px";
 };
+
+function calculateFontSize(width, height, ratio) {
+  let minDimension = Math.min(width, height);
+  return minDimension * ratio;
+}
 
 let startTimer = () => {
   setInterval(() => {
@@ -62,6 +94,13 @@ let startTimer = () => {
 window.onload = () => {
   generateNewCard();
   startTimer();
+
+  document
+    .getElementById("widthInput")
+    .addEventListener("input", updateHeightBasedOnWidth);
+  document
+    .getElementById("heightInput")
+    .addEventListener("input", updateWidthBasedOnHeight);
 };
 
 document
